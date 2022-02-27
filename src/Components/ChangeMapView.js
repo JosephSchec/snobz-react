9180415d5a58d8060c08501cf02366c1aa0f89ec
Joseph-Schechter <shecky522@gmail.com>
2022-02-27 11:53:56 -0500

    change zoom level if loacation is given or not

--- src/Components/ChangeMapView.js
+++ src/Components/ChangeMapView.js
@@ -9,23 +9,30 @@ const usLng = -95.712900;
 export default function ChangeMapView(props) {
     const { newView } = props;
     const [center, setCenter] = useState([usLat, usLng]);
-    
+    const [centerChanged, setCenterChanged] = useState(false);
+
     function getCurr() {
         navigator.geolocation.getCurrentPosition(changeView);
+       
     }
 
     function changeView(pos) {
-        setCenter([pos.coords.latitude, pos.coords.longitude]);
+        setCenter([pos.coords.latitude, pos.coords.longitude]); 
+        setCenterChanged(true);
     }
     useEffect(() => {
         if (newView === '' || newView === null || newView === undefined) {
-            getCurr();
+           getCurr();
         } else if (newView !== center) {
             setCenter(newView);
         }
-    }, [newView])
+    }, [newView,center])
     const map = useMap();
-    map.setView(center, 8);
+    if (centerChanged) {
+        map.setView(center, 8);
+    } else {
+        map.setView(center, 3)
+    }
 
     return null;
 }
