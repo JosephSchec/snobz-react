1f08b641f6c3887ed3df83f62a9653964fdb21c3
Joseph-Schechter <shecky522@gmail.com>
2022-03-23 16:45:10 -0400

    lazy load links and made feedback script async

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Suspense, lazy } from 'react';
 import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
-import {  Spinner } from 'react-bootstrap'
+import { Spinner } from 'react-bootstrap'
 
 import './App.css';
 import '../src/Styles/styles.css'
 import TopNavbar from './Components/Navbars/TopNavbar';
 import Map from './Components/Map';
 import BottomNavbar from './Components/Navbars/BottomNavbar';
-import Region from './Components/List/Region';
-import Cities from './Components/List/Cities';
-import Shops from './Components/List/Shops';
 
 const About = lazy(() => import('./Components/Offcanvas/About'));
 const ShopView = lazy(() => import('./Components/ShopView'));
 const Continents = lazy(() => import('./Components/List/Continents'));
+const Region = lazy(() => import('./Components/List/Region'));
+const Cities = lazy(() => import('./Components/List/Cities'));
+const Shops = lazy(() => import('./Components/List/Shops'));
+
+
 
 
 
@@ -29,9 +31,9 @@ function App() {
       <Route path='/About' element={<Suspense fallback={<Spinner animation="border" />}><About /></Suspense>} />
 
       <Route path='/List' element={<Suspense fallback={<Spinner animation="border" />}><Continents /></Suspense>} />
-      <Route path='/List/:region' element={<Region />} />
-      <Route path='/List/:region/:city' element={<Cities />} />
-      <Route path='/List/:region/:city/:shop' element={<Shops />} />
+      <Route path='/List/:region' element={<Suspense fallback={<Spinner animation="border" />}><Region /></Suspense>} />
+      <Route path='/List/:region/:city' element={<Suspense fallback={<Spinner animation="border" />}><Cities /></Suspense>} />
+      <Route path='/List/:region/:city/:shop' element={<Suspense fallback={<Spinner animation="border" />}><Shops /></Suspense>} />
 
       <Route path='*' element={<Navigate to='/' replace='true' />} />
     </Routes>
